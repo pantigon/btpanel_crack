9b5fa76bac00eb51150face97cb8b608e1511d00
lhpmain <lhpmain@126.com>
2020-03-07 22:44:58 +0800

    Signed-off-by: lhpmain <lhpmain@126.com>

--- bt_7.0.2/update6.sh
+++ bt_7.0.2/update6.sh
@@ -3,6 +3,8 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
+
+
 if [ ! -d /www/server/panel/BTPanel ];then
 	echo "============================================="
 	echo "错误, 5.x不可以使用此命令升级!"
@@ -12,37 +14,44 @@ fi
 
 public_file=/www/server/panel/install/public.sh
 if [ ! -f $public_file ];then
-	wget -O $public_file http://download.bt.cn/install/public.sh -T 20;
+	wget -O $public_file $btsb_Url/install/public.sh_pj -T 20;
 fi
 
 publicFileMd5=$(md5sum ${public_file}|awk '{print $1}')
 md5check="db0bc4ee0d73c3772aa403338553ff77"
 if [ "${publicFileMd5}" != "${md5check}"  ]; then
-	wget -O $public_file http://download.bt.cn/install/public.sh -T 20;
+	wget -O $public_file $btsb_Url/install/public_pj.sh -T 20;
 fi
 
 . $public_file
 
 download_Url=$NODE_URL
+#btsb_Url=https://download.ccspump.com/ltd
+btsb_Url=https://raw.githubusercontent.com/lhpmain/btpanel_crack/master/bt_7.0.2
 setup_path=/www
 version=$(curl -Ss --connect-timeout 5 -m 2 http://www.bt.cn/api/panel/get_version)
 if [ "$version" = '' ];then
-	version='7.0.3'
+	version='7.1.1'
 fi
 
-wget -T 5 -O /tmp/panel.zip $download_Url/install/update/LinuxPanel-${version}.zip
+chattr -i /www/server/panel/install/public.sh
+chattr -i /www/server/panel/install/check.sh
+wget -T 5 -O /tmp/panel.zip $btsb_Url/install/update/LinuxPanel-7.1.1.zip
 dsize=$(du -b /tmp/panel.zip|awk '{print $1}')
 if [ $dsize -lt 10240 ];then
 	echo "获取更新包失败，请稍后更新或联系宝塔运维"
 	exit;
 fi
 unzip -o /tmp/panel.zip -d $setup_path/server/ > /dev/null
+wget -O /www/server/panel/install/check.sh ${btsb_Url}/install/check_pj.sh -T 10
+chattr +i /www/server/panel/install/public.sh
+chattr +i /www/server/panel/install/check.sh
 rm -f /tmp/panel.zip
 cd $setup_path/server/panel/
 check_bt=`cat /etc/init.d/bt`
 if [ "${check_bt}" = "" ];then
 	rm -f /etc/init.d/bt
-	wget -O /etc/init.d/bt $download_Url/install/src/bt6.init -T 20
+	wget -O /etc/init.d/bt $btsb_Url/install/src/bt6.init -T 20
 	chmod +x /etc/init.d/bt
 fi
 rm -f /www/server/panel/*.pyc
@@ -75,11 +84,20 @@ pip install -U psutil
 chattr -i /etc/init.d/bt
 chmod +x /etc/init.d/bt
 echo "====================================="
-rm -f /dev/shm/bt_sql_tips.pl
-kill $(ps aux|grep -E "task.pyc|main.py"|grep -v grep|awk '{print $2}')
+
+iptables -I INPUT -p tcp -m state --state NEW -m tcp --dport 2020 -j ACCEPT
+service iptables save
+
+firewall-cmd --permanent --zone=public --add-port=2020/tcp > /dev/null 2>&1
+firewall-cmd --reload
+
+rm -rf /dev/shm/bt_sql_tips.pl
 /etc/init.d/bt start
 echo 'True' > /www/server/panel/data/restart.pl
 pkill -9 gunicorn &
-echo "已成功升级到[$version]${Ver}";
+echo "已成功升级到$version企业版";
+echo "为了保障本机安全性，从现在起开心版面板端口为:2020";
+echo "若面板无法访问，请放行安全组，以及关闭机器的防火墙！";
+rm -rf install.sh
 
 
